7bbc9abbec1901c0601155c5e78b3b4120611a20
yuumi7372 <morita.yumi707@mail.kyutech.jp>
2026-06-03 11:56:11 +0900

    最初のページ簡易版を作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,24 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="ja"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=WDXL+Lubrifont+JP+N&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
